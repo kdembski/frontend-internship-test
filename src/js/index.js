129f3db27abce1b2fc2ac84ab2697043a52bd0b7
Karol <krldembski@gmail.com>
2021-03-28 21:03:19 +0200

    custom js validation

--- src/js/index.js
+++ src/js/index.js
@@ -1,40 +1,93 @@
-/* Here goes your JS code */
+const showFormBtn = document.getElementById("show-popup-form");
+const closeFormBtn = document.getElementById("close-popup-form");
+const termsCheckmark = document.getElementById("terms-checkmark");
+const termsCheckbox = document.getElementById("terms-checkbox");
+const popup = document.querySelector(".popup");
+const loginForm = document.querySelector(".login-form");
+const formError = document.getElementById("form-error");
+
 //toggle popup
-function togglePopup() {
-  document.querySelector(".popup").classList.toggle("popup_hidden");
-}
-// toggle show form button
-function toggleShowFromButton() {
-  document
-    .getElementById("show-popup-form")
-    .classList.toggle("btn-main_hidden");
-}
-//function on terms checkbox change
-function termsCheckboxChanged() {
-  let termsCheckmark = document.getElementById("terms-checkmark");
+showFormBtn.addEventListener("click", function() {
+  popup.classList.toggle("popup_hidden");
+  this.classList.toggle("btn-main_hidden");
+});
+closeFormBtn.addEventListener("click", () => {
+  popup.classList.toggle("popup_hidden");
+  showFormBtn.classList.toggle("btn-main_hidden");
+  formError.classList.add("login-form__error_hidden");
+});
+
+//checkbox on change
+termsCheckbox.addEventListener("change", () => {
   termsCheckmark.style.borderColor = "grey";
   termsCheckmark.style.backgroundColor = "unset";
+});
+
+//email validation
+function validateEmail(email) {
+  const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  return re.test(email);
 }
-//toggle success message
-function toggleSuccessMessage() {
-  document
-    .querySelector(".success-message")
-    .classList.toggle("success-message_hidden");
+
+//show form error
+function showFormError(message) {
+  if (!formError.classList.contains("login-form__error_hidden")) {
+    formError.classList.add("login-form__error_hidden");
+  } else {
+    formError.classList.remove("login-form__error_hidden");
+    formError.innerHTML = message;
+  }
+  setTimeout(() => {
+    formError.classList.remove("login-form__error_hidden");
+    formError.innerHTML = message;
+  }, 200);
 }
-//function on submit form
-function loginFormSubmited(event) {
-  var termsCheckbox = document.getElementById("terms-checkbox");
+//hide form error
+formError.addEventListener("click", function() {
+  this.classList.add("login-form__error_hidden");
+});
+
+//login form submit
+loginForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  //terms validation
-  if (!termsCheckbox.checked) {
-    let termsCheckmark = document.getElementById("terms-checkmark");
-    termsCheckmark.style.borderColor = "red";
-    termsCheckmark.style.backgroundColor = "rgb(255, 180, 180)";
+  const email = document.getElementById("email-input");
+  const password = document.getElementById("password-input");
+  //validation
+  if (email.value === "") {
+    email.focus();
+    showFormError("You need to enter e-mail address");
     return false;
   }
-  //hide popup show success message
+  if (!validateEmail(email.value)) {
+    email.focus();
+    showFormError("You need to enter valid e-mail address");
+    return false;
+  }
+  if (password.value === "") {
+    password.focus();
+    showFormError("You need to enter password");
+    return false;
+  }
+  if (password.value.length < 8) {
+    password.focus();
+    showFormError("Password need to be at least 8 characters long");
+    return false;
+  }
+  if (!termsCheckbox.checked) {
+    termsCheckmark.style.borderColor = "red";
+    termsCheckmark.style.backgroundColor = "rgb(255, 180, 180)";
+    showFormError("You need to agree to terms & conditions");
+    return false;
+  }
+
+  //hide error
+  formError.classList.add("login-form__error_hidden");
+
+  //hide popup and show success message
   setTimeout(() => {
-    togglePopup();
-    toggleSuccessMessage();
+    popup.classList.toggle("popup_hidden");
+    document
+      .querySelector(".success-message")
+      .classList.toggle("success-message_hidden");
   }, 3000);
-}
+});
