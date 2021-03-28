32ce1cc2210c3e33d21b035cc8576781da2e7dc3
Karol <krldembski@gmail.com>
2021-03-28 15:04:34 +0200

    success message

--- src/js/index.js
+++ src/js/index.js
@@ -15,17 +15,26 @@ function termsCheckboxChanged() {
   termsCheckmark.style.borderColor = "grey";
   termsCheckmark.style.backgroundColor = "unset";
 }
+//toggle success message
+function toggleSuccessMessage() {
+  document
+    .querySelector(".success-message")
+    .classList.toggle("success-message_hidden");
+}
 //function on submit form
 function loginFormSubmited(event) {
   var termsCheckbox = document.getElementById("terms-checkbox");
   event.preventDefault();
+  //terms validation
   if (!termsCheckbox.checked) {
     let termsCheckmark = document.getElementById("terms-checkmark");
     termsCheckmark.style.borderColor = "red";
     termsCheckmark.style.backgroundColor = "rgb(255, 180, 180)";
     return false;
   }
+  //hide popup show success message
   setTimeout(() => {
     togglePopup();
+    toggleSuccessMessage();
   }, 3000);
 }
